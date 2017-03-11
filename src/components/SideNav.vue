8b8bab3cf3449e1392c20f20637b6e6be34cf0c0
Nicholas Achilles <s4317811@student.uq.edu.au>
2017-03-11 13:35:14 +1000

    Added vuejs + webpack + typescript Modified dev build to use inline-css Made production build compile vue css to stylesheets Removed yarn

---
+++ src/components/SideNav.vue
@@ -0,0 +1,33 @@
+<style scoped>
+</style>
+<template>
+    <ul>
+        <li v-for="link in links">
+            <a :href="'#' + link.href">{{ link.text }}</a>
+        </li>
+    </ul>
+</template>
+<script lang="ts">
+    import {
+        Vue,
+        Component,
+        Prop
+    } from 'av-ts'
+
+    @Component
+    export default class SideNav extends Vue {
+        links = [{
+            text: "Home",
+            href: "home"
+        }, {
+            text: "Other Page",
+            href: "other"
+        }, {
+            text: "Some Other Page",
+            href: "someother"
+        }]
+
+        construct() {
+        }
+    }
+</script>
---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import * as Vue from 'vue'
+import App from './App'
+
+import './styles/main.css'
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  render: h => h(App)
+})
