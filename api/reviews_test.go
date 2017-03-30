5b63cd5a7ebad10accd3b09b6741c7ef265b0b12
MrCeleryman <danielisbread@gmail.com>
2017-03-30 18:31:18 +1000

    updated the other tests to use Helper Functions

--- api/reviews_test.go
+++ api/reviews_test.go
@@ -1,27 +1,11 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-	"net/http/httptest"
-	"os"
 	"testing"
 
 	_ "github.com/mattn/go-sqlite3"
 )
 
 func TestGetReviews(t *testing.T) {
-	os.Setenv("TEST", "1")
-	db := InitDb()
-	defer db.Close()
-	testRouter := SetupRouter(true, false)
-	request, err := http.NewRequest("GET", "/api/v1/reviews/", nil)
-	if err != nil {
-		fmt.Println(err)
-	}
-	response := httptest.NewRecorder()
-	testRouter.ServeHTTP(response, request)
-	if response.Code != 200 {
-		t.Errorf("Expected 200")
-	}
+	GetFunc(t, "/api/v1/reviews/", 200)
 }
--- api/venues_test.go
+++ api/venues_test.go
@@ -1,27 +1,11 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-	"net/http/httptest"
-	"os"
 	"testing"
 
 	_ "github.com/mattn/go-sqlite3"
 )
 
 func TestGetVenues(t *testing.T) {
-	os.Setenv("TEST", "1")
-	db := InitDb()
-	defer db.Close()
-	testRouter := SetupRouter(true, false)
-	request, err := http.NewRequest("GET", "/api/v1/venues/", nil)
-	if err != nil {
-		fmt.Println(err)
-	}
-	response := httptest.NewRecorder()
-	testRouter.ServeHTTP(response, request)
-	if response.Code != 200 {
-		t.Errorf("Expected 200")
-	}
+	GetFunc(t, "/api/v1/venues/", 200)
 }
